d4e7090e1aa062c2307621e891a74aeb00fc388e
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-08-30 20:54:19 +0800

    Uncomment js code

--- resellerAdminB/assets/js/edit-keyword-filter.js
+++ resellerAdminB/assets/js/edit-keyword-filter.js
@@ -19,34 +19,34 @@ $('input[name="username"]').on('change', function () {
 
 // Add new spam word
 function editSpamFilterSubmit(id) {
-    //var el = document.getElementById(id)
-    //el.disabled = true;
+    var el = document.getElementById(id)
+    el.disabled = true;
 
-    //var spamWord = document.getElementById("spamWord");
-    //var spamStatus = document.getElementById("spamStatus");
-    //var filterSpamWordUsername = document.getElementById("filterSpamWordUsername");
-    //var username = document.querySelector('input[name="username"]:checked');
+    var spamWord = document.getElementById("spamWord");
+    var spamStatus = document.getElementById("spamStatus");
+    var filterSpamWordUsername = document.getElementById("filterSpamWordUsername");
+    var username = document.querySelector('input[name="username"]:checked');
 
-    //if (spamWord.value.trim() == "") {
-    //    toastr.error("Please enter spam keyword");
-    //    el.disabled = false;
-    //    return false;
-    //}
+    if (spamWord.value.trim() == "") {
+        toastr.error("Please enter spam keyword");
+        el.disabled = false;
+        return false;
+    }
 
-    //if (spamStatus.value.trim() == "") {
-    //    toastr.error("Please select spam keyword status");
-    //    el.disabled = false;
-    //    return false;
-    //}
+    if (spamStatus.value.trim() == "") {
+        toastr.error("Please select spam keyword status");
+        el.disabled = false;
+        return false;
+    }
 
-    //if (username.value == "CustomUsername") {
-    //    if (filterSpamWordUsername.value.trim() == "") {
-    //        toastr.error("Please enter custom username");
-    //        el.disabled = false;
-    //        return false;
-    //    }
-    //}
+    if (username.value == "CustomUsername") {
+        if (filterSpamWordUsername.value.trim() == "") {
+            toastr.error("Please enter custom username");
+            el.disabled = false;
+            return false;
+        }
+    }
 
-    //el.disabled = false;
+    el.disabled = false;
     __doPostBack(id, '');
 }
--- resellerAdminB/assets/js/edit-sender-filter.js
+++ resellerAdminB/assets/js/edit-sender-filter.js
@@ -18,34 +18,34 @@ $('input[name="username"]').on('change', function () {
 
 // Edit sender name filter
 function editSenderFilterSubmit(id) {
-    //var el = document.getElementById(id)
-    //el.disabled = true;
+    var el = document.getElementById(id)
+    el.disabled = true;
 
-    //var senderName = document.getElementById("senderName");
-    //var senderStatus = document.getElementById("senderStatus");
-    //var filterSenderName = document.getElementById("filterSenderName");
-    //var username = document.querySelector('input[name="username"]:checked');
+    var senderName = document.getElementById("senderName");
+    var senderStatus = document.getElementById("senderStatus");
+    var filterSenderName = document.getElementById("filterSenderName");
+    var username = document.querySelector('input[name="username"]:checked');
 
-    //if (senderName.value.trim() == "") {
-    //    toastr.error("Please enter Sender name");
-    //    el.disabled = false;
-    //    return false;
-    //}
+    if (senderName.value.trim() == "") {
+        toastr.error("Please enter Sender name");
+        el.disabled = false;
+        return false;
+    }
 
-    //if (senderStatus.value.trim() == "") {
-    //    toastr.error("Please select sender status");
-    //    el.disabled = false;
-    //    return false;
-    //}
+    if (senderStatus.value.trim() == "") {
+        toastr.error("Please select sender status");
+        el.disabled = false;
+        return false;
+    }
 
-    //if (username.value == "CustomUsername") {
-    //    if (filterSenderName.value.trim() == "") {
-    //        toastr.error("Please enter custom username");
-    //        el.disabled = false;
-    //        return false;
-    //    }
-    //}
+    if (username.value == "CustomUsername") {
+        if (filterSenderName.value.trim() == "") {
+            toastr.error("Please enter custom username");
+            el.disabled = false;
+            return false;
+        }
+    }
 
-    //el.disabled = false;
+    el.disabled = false;
     __doPostBack(id, '');
 }
